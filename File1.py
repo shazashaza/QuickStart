15f350153660101d30792159be3ca8ef8e97da61
Shaza Hamdan <shaza.hamdan@gmail.com>
2024-11-21 09:34:14 +0300

    Changed in File1 by Person1 in dev1

--- File1.py
+++ File1.py
@@ -5,7 +5,7 @@ def write_to_file():
         file.write("Hello, this is a test file.\n")
         file.write("This file contains some simple data.")
         file.write("This file contains some simple data. in Person2")
-
+        file.write("Changes from dev1")
 if __name__ == "__main__":
     write_to_file()
     print("Data has been written to the file.")
