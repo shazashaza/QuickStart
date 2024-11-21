153137a4cbab72ca8104ce499e23c942a9bd32e1
Shaza Hamdan <shaza.hamdan@gmail.com>
2024-11-21 09:48:46 +0300

    Commit changes before switching branches

--- additional_feature.py
+++ additional_feature.py
@@ -1,6 +1,6 @@
 # new_feature.py
 def greet():
-    print("Hello from the dev branch in Person1!")
+    print("Hello from the dev branch in Person2!")
     print("minor change")
 if __name__ == "__main__":
     greet()
